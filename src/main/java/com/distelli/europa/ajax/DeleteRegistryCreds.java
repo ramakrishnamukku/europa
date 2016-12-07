8bbfac1c2f3f9ef6fd9eb88afab215e9bca37cfe
Sam Heutmaker <sam@sam.local>
2016-12-07 15:44:51 -0800

    Fix typo

--- src/main/java/com/distelli/europa/ajax/DeleteRegistryCreds.java
+++ src/main/java/com/distelli/europa/ajax/DeleteRegistryCreds.java
@@ -39,8 +39,8 @@ public class DeleteRegistryCreds implements AjaxHelper
                                                           RegistryProvider.class,
                                                           true); //throw if missing
         String region = ajaxRequest.getParam("region", true);
-        // String name = ajaxRequest.getParam("name", true);
-        _db.deleteCred(provider, region);
+        String name = ajaxRequest.getParam("name", true);
+        _db.deleteCred(provider, region, name);
         return JsonSuccess.Success;
     }
 }
