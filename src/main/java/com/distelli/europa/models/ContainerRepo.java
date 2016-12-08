ee0ae7e2be491cba32b86de563e7eb373aae145e
Rahul Singh <rsingh@distelli.com>
2016-12-08 11:33:52 -0800

    Added credName to the container repo

--- src/main/java/com/distelli/europa/models/ContainerRepo.java
+++ src/main/java/com/distelli/europa/models/ContainerRepo.java
@@ -3,15 +3,11 @@ package com.distelli.europa.models;
 import lombok.*;
 
 @Data
+@Builder
 public class ContainerRepo
 {
     protected String name = null;
+    protected String credName = null;
     protected RegistryProvider provider = null;
     protected String region = null;
-
-    @Builder
-    public ContainerRepo()
-    {
-
-    }
 }
