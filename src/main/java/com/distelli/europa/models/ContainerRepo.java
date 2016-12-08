434acc3632a6e7346c497fef1ce25c038d4ba618
Rahul Singh <rsingh@distelli.com>
2016-12-08 11:36:20 -0800

    Added default constructor back to container repo

--- src/main/java/com/distelli/europa/models/ContainerRepo.java
+++ src/main/java/com/distelli/europa/models/ContainerRepo.java
@@ -3,11 +3,16 @@ package com.distelli.europa.models;
 import lombok.*;
 
 @Data
-@Builder
 public class ContainerRepo
 {
     protected String name = null;
     protected String credName = null;
     protected RegistryProvider provider = null;
     protected String region = null;
+
+    @Builder
+    public ContainerRepo()
+    {
+
+    }
 }
