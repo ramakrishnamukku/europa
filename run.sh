b3ad43eabea4f175ad477b2fef4aa8682a3f2764
Rahul Singh <rsingh@distelli.com>
2016-12-29 12:31:57 -0800

    Added JVM args to the run.sh

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 DEPS_CLASSPATH=`cat target/.classpath`
 CLASSPATH=target/classes/:$DEPS_CLASSPATH
-
+JVM_ARGS="-Duser.timezone=UTC -Xmx2000M -Xms2000M"
 $JAVA_HOME/bin/java $JVM_ARGS -cp $CLASSPATH -Duser.timezone=UTC com.distelli.europa.Europa --port 5050 --log-to-console --config EuropaConfig.json $@
