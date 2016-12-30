57752ad8b2c7167b520b8b5e2249f7502458f564
Brian Maher <brian@brimworks.com>
2016-12-30 10:35:51 -0800

    Use the EUROPA_CONFIG environment variable to launch the server with the proper config.

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -e
 
+if [ -z "$EUROPA_CONFIG" ]; then
+    echo "You must set the EUROPA_CONFIG environment variable to point to the configuration file." 1>&2
+    exit 1
+fi
+
 DEPS_CLASSPATH=`cat target/.classpath`
 CLASSPATH=target/classes/:$DEPS_CLASSPATH
 JVM_ARGS="-Duser.timezone=UTC -Xmx2000M -Xms2000M"
-$JAVA_HOME/bin/java $JVM_ARGS -cp $CLASSPATH -Duser.timezone=UTC com.distelli.europa.Europa --port 5050 --log-to-console --config EuropaConfig.json $@
+$JAVA_HOME/bin/java $JVM_ARGS -cp $CLASSPATH -Duser.timezone=UTC com.distelli.europa.Europa --port 5050 --log-to-console --config "$EUROPA_CONFIG" $@
