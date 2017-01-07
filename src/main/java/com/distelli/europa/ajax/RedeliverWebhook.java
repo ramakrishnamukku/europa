78f53c5808d2577019a7ed31b86b845df90ec32d
Rahul Singh <rsingh@distelli.com>
2017-01-06 17:55:03 -0800

    Save the notificationId with the event on redelivery

--- src/main/java/com/distelli/europa/ajax/RedeliverWebhook.java
+++ src/main/java/com/distelli/europa/ajax/RedeliverWebhook.java
@@ -9,16 +9,19 @@
 package com.distelli.europa.ajax;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.persistence.EntityNotFoundException;
 
+import com.distelli.europa.db.*;
 import com.distelli.europa.models.*;
 import com.distelli.europa.notifiers.*;
 import com.distelli.europa.util.*;
+import com.distelli.objectStore.*;
 import com.distelli.webserver.HTTPMethod;
 import com.distelli.webserver.JsonError;
-import com.distelli.objectStore.*;
 
 import lombok.extern.log4j.Log4j;
 
@@ -32,6 +35,8 @@ public class RedeliverWebhook extends AjaxHelper
     private ObjectKeyFactory _objectKeyFactory;
     @Inject
     private ObjectStore _objectStore;
+    @Inject
+    private RepoEventsDb _repoEventsDb;
 
     public RedeliverWebhook()
     {
@@ -42,10 +47,21 @@ public class RedeliverWebhook extends AjaxHelper
     {
         String id = ajaxRequest.getParam("notificationId",
                                          true); //throw if missing
+        String eventId = ajaxRequest.getParam("eventId",
+                                         true); //throw if missing
+        String repoId = ajaxRequest.getParam("repoId",
+                                             true); //throw if missing
+        String domain = null;
         NotificationId notificationId = NotificationId.fromCanonicalId(id);
         NotificationType type = notificationId.getType();
         if(type != NotificationType.WEBHOOK)
             throw(new AjaxClientException("Invalid WebhookId: "+id, JsonError.Codes.BadContent, 400));
+
+        //now get the event
+        RepoEvent event = _repoEventsDb.getEventById(domain, repoId, eventId);
+        if(event == null)
+            throw(new AjaxClientException("No such event: "+eventId, JsonError.Codes.BadContent, 400));
+
         ObjectKey objectKey = _objectKeyFactory.forWebhookRecord(notificationId);
         WebhookRecord record = null;
         try {
@@ -56,7 +72,15 @@ public class RedeliverWebhook extends AjaxHelper
         } catch(IOException ioe) {
             throw(new RuntimeException(ioe));
         }
+
         NotificationId nfId = _webhookNotifier.notify(record);
-        return nfId.toCanonicalId();
+        String canonicalNfId = nfId.toCanonicalId();
+        List<String> nfIdList = event.getNotifications();
+        if(nfIdList == null)
+            nfIdList = new ArrayList<String>();
+        nfIdList.add(canonicalNfId);
+        _repoEventsDb.setNotifications(event.getDomain(), event.getRepoId(), event.getId(), nfIdList);
+
+        return canonicalNfId;
     }
 }
