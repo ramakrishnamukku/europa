35c1101c71324c384ad4277647960c8c006dbb46
Sam Heutmaker <samheutmaker@gmail.com>
2017-01-10 11:41:28 -0800

    Load favicon.png as favicon

--- src/main/java/com/distelli/europa/react/PageTemplate.java
+++ src/main/java/com/distelli/europa/react/PageTemplate.java
@@ -31,6 +31,7 @@ public class PageTemplate
     "  <head>"+
     "    <meta charset=\"utf-8\">"+
     "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">"+
+    "    <link rel=\"icon\" type=\"image/png\" href=\"/public/favicon.png\" />"+
     "    <title>Europa Container Registry</title>"+
     "  </head>"+
     "  <body>"+
