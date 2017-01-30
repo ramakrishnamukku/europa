1d0fec8b1cd646cca3e32fd8408260bfa567d343
Brian Maher <brian@brimworks.com>
2017-01-30 09:14:37 -0800

    Do NOT list sha256: tags in RegistryTagList handler.

--- src/main/java/com/distelli/europa/handlers/RegistryTagList.java
+++ src/main/java/com/distelli/europa/handlers/RegistryTagList.java
@@ -1,29 +1,32 @@
 package com.distelli.europa.handlers;
 
 import com.distelli.europa.EuropaRequestContext;
-import org.eclipse.jetty.http.HttpMethod;
-import com.distelli.webserver.RequestHandler;
-import com.distelli.webserver.WebResponse;
-import com.distelli.webserver.RequestContext;
-import com.fasterxml.jackson.databind.JsonNode;
-import javax.inject.Singleton;
-import java.util.Map;
-import java.util.HashMap;
-import javax.inject.Inject;
-import lombok.extern.log4j.Log4j;
 import com.distelli.europa.db.RegistryManifestDb;
+import com.distelli.europa.models.ContainerRepo;
 import com.distelli.europa.models.RegistryManifest;
-import java.util.List;
 import com.distelli.europa.registry.RegistryError;
 import com.distelli.europa.registry.RegistryErrorCode;
-import java.util.stream.Collectors;
 import com.distelli.persistence.PageIterator;
-import com.distelli.europa.models.ContainerRepo;
+import com.distelli.webserver.RequestContext;
+import com.distelli.webserver.RequestHandler;
+import com.distelli.webserver.WebResponse;
+import com.fasterxml.jackson.databind.JsonNode;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import lombok.extern.log4j.Log4j;
+import org.eclipse.jetty.http.HttpMethod;
 
 @Log4j
 @Singleton
 public class RegistryTagList extends RegistryBase {
+    private static final Pattern SHA256_PATTERN = Pattern.compile("^sha256:[0-9a-fA-F]{64}$");
     @Inject
     private RegistryManifestDb _manifestDb;
     private static class Response {
@@ -46,7 +49,12 @@ public class RegistryTagList extends RegistryBase {
         PageIterator it = new PageIterator()
             .pageSize(getPageSize(requestContext))
             .marker(requestContext.getParameter("last"));
-        List<RegistryManifest> manifests = _manifestDb.listManifestsByRepoId(ownerDomain, repo.getId(), it);
+        List<RegistryManifest> manifests = new ArrayList<>();
+        while ( it.hasNext() && manifests.size() < it.getPageSize() ) {
+            _manifestDb.listManifestsByRepoId(ownerDomain, repo.getId(), it).stream()
+                .filter((manifest) -> !SHA256_PATTERN.matcher(manifest.getTag()).matches())
+                .forEachOrdered(manifests::add);
+        }
 
         Response response = new Response();
         response.name = joinWithSlash(ownerUsername, name);
