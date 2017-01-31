728d998a6aa5c9d126990dc786af9c6798be6c47
Brian Maher <brian@brimworks.com>
2017-01-31 10:06:59 -0800

    Docker container listens on 443 for SSL, ./run.sh listens on port 5443 for SSL.

--- run.sh
+++ run.sh
@@ -7,4 +7,4 @@ fi
 DEPS_CLASSPATH=`cat target/.classpath`
 CLASSPATH=target/classes/:$DEPS_CLASSPATH
 JVM_ARGS="-Duser.timezone=UTC -Xmx2000M -Xms2000M"
-$JAVA_HOME/bin/java $JVM_ARGS -cp $CLASSPATH com.distelli.europa.Europa --stage "$STAGE" --port 5050 --log-to-console --config "$EUROPA_CONFIG" $@
+$JAVA_HOME/bin/java $JVM_ARGS -cp $CLASSPATH com.distelli.europa.Europa --stage "$STAGE" --port 5050 --ssl-port 5443 --log-to-console --config "$EUROPA_CONFIG" $@
