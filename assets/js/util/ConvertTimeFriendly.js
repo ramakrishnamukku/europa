feac9fdd1e88213d3c3a2ac300325332c3f4b1f2
Sam Heutmaker <samheutmaker@gmail.com>
2017-02-10 15:01:22 -0800

    Update ConvertFriendyTime for month support

--- assets/js/util/ConvertTimeFriendly.js
+++ assets/js/util/ConvertTimeFriendly.js
@@ -1,9 +1,18 @@
-// Convert milliseconds since epoch to friendly time i.e. "123456789" => "x days ago"
+/*
+  @author Sam Heutmaker [samheutmaker@gmail.com]
+*/
+
+// Convert milliseconds since epoch to friendly time i.e. "123456789" => "x months ago"
 
 export default function(millis){
   var seconds = Math.floor((new Date() - millis) / 1000);
-  var interval = Math.floor(seconds / 86400);
+  var interval = Math.floor(seconds / 2592000);
 
+  if(interval >= 1) {
+    return interval + " month(s) ago";
+  }
+
+  interval  = Math.floor(seconds / 86400);
   if (interval >= 1) {
     return interval + " day(s) ago";
   }
