5f1b6fe9eb62fcae33b2cb8c99b55fc4417be031
Rahul Singh <rsingh@distelli.com>
2017-02-13 14:11:31 -0800

    Throw a RegistryError if the name is unknown

--- src/main/java/com/distelli/europa/handlers/RegistryDefault.java
+++ src/main/java/com/distelli/europa/handlers/RegistryDefault.java
@@ -1,23 +1,26 @@
 package com.distelli.europa.handlers;
 
-import com.distelli.europa.EuropaRequestContext;
+import java.util.HashMap;
+import java.util.Map;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
 import org.eclipse.jetty.http.HttpMethod;
+import com.distelli.europa.EuropaRequestContext;
+import com.distelli.europa.registry.RegistryError;
+import com.distelli.europa.registry.RegistryErrorCode;
+import com.distelli.webserver.RequestContext;
 import com.distelli.webserver.RequestHandler;
 import com.distelli.webserver.WebResponse;
-import com.distelli.webserver.RequestContext;
 import com.fasterxml.jackson.databind.JsonNode;
-import javax.inject.Singleton;
-import java.util.Map;
-import java.util.HashMap;
-import javax.inject.Inject;
+
 import lombok.extern.log4j.Log4j;
 
 @Log4j
 @Singleton
 public class RegistryDefault extends RegistryBase {
     public WebResponse handleRegistryRequest(EuropaRequestContext requestContext) {
-        log.info("404: "+requestContext);
-        WebResponse response = new WebResponse(404);
-        return response;
+        throw(new RegistryError("Invalid or unknown name",
+                                RegistryErrorCode.NAME_UNKNOWN));
     }
 }
